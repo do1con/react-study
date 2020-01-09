3cd7ca6f28730077a1d88e7768897c57b6ec17d6
SeongsooKim <kss7547@gmail.com>
2020-01-09 18:42:49 +0900

    SPA - 모듈 없이 windows.history API로 구현해보기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
-import React from 'react';
-import Button from './button2';
-import Box from './box1';
-import Box3 from './box3';
+import React, { Component } from 'react'
 
-
-function App() {
-  return (
-    <div>
-      <Button size="big"></Button>
-      <Button size="small"></Button>
-      <Box size="big"></Box>
-      <Box size="small"></Box>
-      <Box3 size='big'></Box3>
-      <Box3 size='small'></Box3>
-    </div>
-  );
+class App extends Component {
+  state = {
+    pageName: ''
+  };
+  componentDidMount () {
+    window.onpopstate = event => {
+      this.onChangePage(event.state);
+      console.log(this.state);
+      console.log(window.history);
+    }
+  }
+  onChangePage = pageCode => {
+    this.setState({pageName: pageCode});
+  }
+  onClick1 = () => {
+    const pageName = 'page1';
+    window.history.pushState(pageName, '', '/page1');
+    this.onChangePage(pageName);
+  }
+  onClick2 = () => {
+    const pageName = 'page2';
+    window.history.pushState(pageName, '', '/page2');
+    this.onChangePage(pageName);
+  }
+  render() {
+    const { pageName } = this.state;
+    return (
+      <div>
+        <button onClick={this.onClick1}>page1</button>
+        <button onClick={this.onClick2}>page2</button>
+        {!pageName&&<Home/>}
+        {pageName === 'page1' && <Page1 />}
+        {pageName === 'page2' && <Page2 />}
+      </div>
+    )
+  }
+}
+function Home () {
+  return <h2>원하는 곳 클릭</h2>;
+}
+function Page1 () {
+  return <h2>page2</h2>;
+}
+function Page2 () {
+  return <h2>page1</h2>;
 }
 
 export default App;
