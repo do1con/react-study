d658b9be72b08adc8108346cfb5d984cdb7a8d6c
SeongsooKim <kss7547@gmail.com>
2020-01-09 12:01:54 +0900

    간단 투두리스트, {} 의 의미, 리액트만 임포트 말고 컴포넌트도 임포트해야 컴포넌트 작성가능, 컴포넌트는 객체, 객체와 변수차이

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { addNumber } from './util';
+import TodoList from './TodoList';
 
 function App() {
   addNumber(1, 2);
   return (
     <div className="App">
       {addNumber(1,2)}
+      <TodoList />
     </div>
   );
 }
