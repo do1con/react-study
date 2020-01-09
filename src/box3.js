7219443430d8fc3bbd70b8e4cf4dc2b1b5cfbe47
SeongsooKim <kss7547@gmail.com>
2020-01-09 17:44:58 +0900

    리액트스럽게 생각하기, UI 인터랙션을 반드시 이벤트함수로 처리할 필요없음.

--- src/box3.js
+++ src/box3.js
@@ -2,22 +2,14 @@ import React from "react";
 import styled from 'styled-components';
 
 const BoxCommon = styled.div`
+  width: ${props => (props.isBig ? 200 : 100)}px;
   height: 50px;
   background-color: #aaaaaa;
 `;
-const BoxBig = styled(BoxCommon)`
-  width: 200px;
-`;
-const BoxSmall = styled(BoxCommon)`
-  width: 100px;
-`;
-
-const BoxBigClickHandler = () => {
-  console.log('big box');
-}
-const BoxSmallClickHandler = () => console.log('small box');
 
 function Box({ size }) {
-  return size === 'big' ? <BoxBig onClick={BoxBigClickHandler}>큰 박스</BoxBig> : <BoxSmall onClick={BoxSmallClickHandler}>작은 박스</BoxSmall>;
+  const isBig = size === 'big';
+  const label = isBig ? '큰 박스' : '작은 박스';
+  return <BoxCommon isBig={isBig}>{label}</BoxCommon>
 }
 export default Box;
