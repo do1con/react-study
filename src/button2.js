05c7891a7ba10ad7c86319ea3656210511f88aa6
SeongsooKim <kss7547@gmail.com>
2020-01-09 16:28:26 +0900

    css 적용법 3 - ~.module.scss로 적용, scss문법 (변수등) 적용가능, scss끼리 import 가능, node-sass 필요

--- src/button2.js
+++ src/button2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./button2.module.css";
+import style from "./button2.module.scss";
 
 function Button({ size }) {
   if (size === "big") return <button className={`${style.button} ${style.big}`}>큰 버튼</button>;
