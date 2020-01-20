725466bf996753797dcd5399fc37562b5a445fe1
SeongsooKim <kss7547@gmail.com>
2020-01-20 12:19:23 +0900

    Hooks 3. - useEffect로 componentDidMount, componentWillUnmount 구현, (구글링 추천)

--- src/useHook.jsx
+++ src/useHook.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
 export default function Profile() {
-  const [name, setName] = useState(''); // 왼쪽이 상탯값 이름, 오른쪽이 변경 메소드명
+  const [width, setWidth] = useState(window.innerWidth);
   useEffect(() => {
-    document.title = `이름 : ${name}`;
-  });
+    const onResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', onResize);
+    return () => { // 첫번째 매개변수의 리턴은, 언마운트되거나 첫번째 함수가 호출되기 직전에 호출된다.
+      window.removeEventListener('resize', onResize);
+    }
+  }, []); // useEffect 두번째에 빈 배열을 넣으면, render후 한번만 실행한다. 즉 (ComponentDidMount)의 역할을 한다.
   return (
     <div>
-      <p>{`name is ${name}`}</p>
-      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      {`width is ${width}`};
     </div>
   );
 }
