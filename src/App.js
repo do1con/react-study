04eb34941fb093d7f406c400030752c851c7efbb
SeongsooKim <kss7547@gmail.com>
2020-01-20 11:28:38 +0900

    Hooks 1. - useState

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
 import React, { Component } from 'react';
-
-const UserContext = React.createContext('unknown');
+import Profile from './useState';
 
 export default class App extends Component {
-
-  state = [
-    {
-      title: 'hi',
-      pri: 'low'
-    },
-    {
-      title: 'hello',
-      pri: 'high'
-    }
-  ];
-
-  componentDidMount (){
-    const { prevState } = this.state;
-    const copy = this.state;
-    console.log(this.state);
-    console.log(copy);
-    copy.push({ title: 'bye', pri: 'middle' });
-    this.setState({ copy });
-    console.log( prevState === this.state );
-  }
 
   render() {
     return (
       <div>
-        <UserContext.Provider value="mike">
-          <div>상단</div>
-          <Profile />
-          <div>하단</div>
-        </UserContext.Provider>
+        <Profile />
       </div>
     );
   }
-}
-
-function Profile(){
-  return (
-    <div>
-      <Greeting />
-    </div>
-  );
-}
-
-function Greeting(){
-  return (
-    <UserContext.Consumer>
-      {username => <p>{`${username}님 안녕하세요`}</p>}
-    </UserContext.Consumer>
-  );
 }
