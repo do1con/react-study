d154ead6f5330f1a4786e1fb7ff043fd8acf487d
SeongsooKim <kss7547@gmail.com>
2020-01-20 15:59:28 +0900

    잠깐... html5/css3/js 보내는 코드... 프로젝트...

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import Profile from './useHook';
+//import useWindowWidth from './useWindowWidth';
+
+const getValue = (values) => values;
 
 export default class App extends Component {
-
+  
   render() {
+    const theHtml = ReactDOMServer.renderToStaticMarkup(<Profile />);
+    console.log(theHtml);
+//    const theHtml2 = reactDOM.render(<Profile />);
+//    console.log(theHtml2);
     return (
       <div>
-        <Profile />
+        <Profile getValue={getValue}/>
+        <br />
       </div>
     );
   }
--- src/useHook.jsx
+++ src/useHook.jsx
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 export default function Profile() {
   const [width, setWidth] = useState(window.innerWidth);
+  const box = {
+    backgroundColor: 'red',
+    width: (width-500)+"px"
+  }
+  const boxJS = () =>{
+    alert(box);
+  }
+  
+  console.log(window[boxJS]);
+  const testBoxJS = String(boxJS)
+  console.log(testBoxJS);
   useEffect(() => {
     const onResize = () => setWidth(window.innerWidth);
     window.addEventListener('resize', onResize);
@@ -9,9 +20,10 @@ export default function Profile() {
       window.removeEventListener('resize', onResize);
     }
   }, []); // useEffect 두번째에 빈 배열을 넣으면, render후 한번만 실행한다. 즉 (ComponentDidMount)의 역할을 한다.
+  
   return (
-    <div>
-      {`width is ${width}`};
+    <div className="hihi" style={box} onClick={boxJS}>
+      {`width is ${width}`}
     </div>
   );
 }
