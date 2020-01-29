1aaf45aa42f3bea416f84f5a42a77db8e7870eaa
SeongsooKim <kss7547@gmail.com>
2020-01-29 18:52:15 +0900

    Revert "리액트훅 - useReducer 이긴 한데 아직 useState와 차이를 못느낌. 다음예제 ㄱ"
    This reverts commit d53ed0b90f1dc55ad48e5b010d944f5fef532de4.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import UseReducer from './UseReducer';
+import React, { useMemo } from 'react';
+import { User } from './Ver1';
+import { User2 } from './Ver2';
 
 
 const App = () => {
 
+  const value = useMemo(() => {
+
+  });
+
   return (
     <div>
-      <h2>UseReducer</h2>
-      <UseReducer />
+      <h3>ver 1 ( useEffect, useState 만으로 만든 것 )</h3>
+      <User />
+      <h3>ver 2 ( useMemo를 활용한 것 )</h3>
+      <User2 />
     </div>
   );
 
