5aa5d6833c53daf7ab3e219da1c5d34f94371911
SeongsooKim <kss7547@gmail.com>
2020-01-29 18:39:44 +0900

    리액트훅 - useReducer 이긴 한데 아직 useState와 차이를 못느낌. 다음예제 ㄱ --- 추가 --- 유즈리듀서가 리덕스처럼 전역적으로 상태값을 관리하는 훅인줄 알았는데 아니었음. 유즈 리듀서 + 콘텍스트API가 합쳐져서 그냥 그런식으로 된다~ 같은 방식이었음. 유즈 리듀서 자체가 뭔지는 이 예제로 충분함

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, { useMemo } from 'react';
-import { User } from './Ver1';
-import { User2 } from './Ver2';
+import React from 'react';
+import UseReducer from './UseReducer';
 
 
 const App = () => {
 
-  const value = useMemo(() => {
-
-  });
-
   return (
     <div>
-      <h3>ver 1 ( useEffect, useState 만으로 만든 것 )</h3>
-      <User />
-      <h3>ver 2 ( useMemo를 활용한 것 )</h3>
-      <User2 />
+      <h2>UseReducer</h2>
+      <UseReducer />
     </div>
   );
 
