45e721cd669f3d576bbe7b841b0a93238f623022
SeongsooKim <kss7547@gmail.com>
2020-02-03 15:43:34 +0900

    immer를 이용해서 리듀서 작성 (새로운 객체를 반환해서 불변성을 지키고 깊은 곳의 객체도 쉽게 변경)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
+import produce from 'immer';
 
-
-const middleware1 = store => next => action => {
-  console.log('middleware1 start');
-  const result = next(action);
-  console.log('middleware1 end');
-  return result;
-}
-
-const middleware2 = store => next => action => {
-  console.log('middleware2 start');
-  const result = next(action);
-  console.log('middleware2 end');
-  return result;
-}
-
-const myReducer = (state, action) => {
-  console.log('my Reducer');
-  return state;
-}
-
-const store = createStore(myReducer, applyMiddleware(middleware1, middleware2));
-store.dispatch({ type: 'someAction' });
-
+const person = { name: 'sskim', age: 24 };
+const newPerson = produce(person, draft => {
+  draft.age = 32;
+});
 
 const App = () => {
 
   return (
     <div>
-      <h4>콘솔, 소스를 보시오.</h4>
+      <h4>콘솔, 소스</h4>
     </div>
   );
 
